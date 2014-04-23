b731e54d1b4c9605543a711de35dd4d97ecf4131
joni2back <joni2back@gmail.com>
2014-04-23 09:55:45 -0300

    updated example with getters and setters

--- examples/InstanceBuilder.php
+++ examples/InstanceBuilder.php
@@ -20,7 +20,7 @@ $namespaceClass = InstanceBuilder::build('NamespaceClass', $properties, 'Core\Sp
 ?>
 
 
-<style>pre {background:#eee;padding:5px;max-width:50em}</style>
+<style>pre {background:#eee;padding:5px;max-width:50em; margin-top:-20px}</style>
 <h4>Class generation on the fly</h4>
 
 
@@ -32,5 +32,21 @@ $namespaceClass = InstanceBuilder::build('NamespaceClass', $properties, 'Core\Sp
 <pre><?php print_r($crazyClass); ?></pre>
 
 
-<h5>InstanceBuilder::build(&#39;NamespaceClass&#39;, $properties, (&#39;Core\Space&#39;);</h5>
+<h5>InstanceBuilder::build(&#39;NamespaceClass&#39;, $properties, &#39;Core\Space&#39;);</h5>
+<pre><?php print_r($namespaceClass); ?></pre>
+
+<h5>$instance-&gt;user;</h5>
+<pre><?php print_r($namespaceClass->getUser()); ?></pre>
+
+<h5>$instance-&gt;getUser();</h5>
+<pre><?php print_r($namespaceClass->getUser()); ?></pre>
+
+<h5>$instance-&gt;getAge();</h5>
+<pre><?php print_r($namespaceClass->getAge()); ?></pre>
+
+<h5>$instance-&gt;getData();</h5>
+<pre><?php print_r($namespaceClass->getData()); ?></pre>
+
+<h5>$instance-&gt;setData($instance-&gt;getData() + array(&#39;lang&#39; =&gt; &#39;es&#39;, &#39;rand&#39; =&gt; rand()));</h5>
+<?php $namespaceClass->setData($namespaceClass->getData() + array('lang' => 'es', 'rand' => rand())); ?>
 <pre><?php print_r($namespaceClass); ?></pre>
